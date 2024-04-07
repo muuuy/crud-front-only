7cbd2b156c2fe3a23eb1ae3306bcca96fe447cab
Matthew Yu <myu2@umbc.edu>
2024-04-07 14:32:30 -0400

    saved deck shit

--- script.js
+++ script.js
@@ -4,28 +4,50 @@ let container = document.querySelector('.container');
 
 let cardContainer = document.querySelector('.card_container');
 
+let createBtn = document.querySelector('.create_deck');
+
 let deckId;
 
-const generateId = () => {
+const onStart = async () => {
+    deckId = localStorage.getItem('deckId');
+    console.log(deckId);
+
+    if( deckId == null) {
+        newDeck("new");
+    } else {
+        newDeck(deckId);
+    }
 }
 
-const newDeck = async () => {
+const newDeck = async (id) => {
     try{
-        let card = await fetch("https://www.deckofcardsapi.com/api/deck/new/draw/?count=2");
+        let card = await fetch(`https://www.deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
         let cardData = await card.json();
 
         console.log(cardData);
 
-        let cardImage = document.createElement('img');
-        cardImage.src = cardData.cards[0].image;
-        console.log(cardData.cards[0].image)
-
-        deckId = cardData.deck_id;
-
-        cardContainer.appendChild(cardImage);
+        addImage(cardData);
     } catch(error) {
         alert(error);
     }   
 }
 
-newDeck();
+const addImage = (cardData) => {
+    let cardImage = document.createElement('img');
+    cardImage.src = cardData.cards[0].image;
+    console.log(cardData.cards[0].image)
+
+    deckId = cardData.deck_id;
+    localStorage.setItem('deckId', deckId);
+
+    cardContainer.appendChild(cardImage);
+}
+
+createBtn.addEventListener('click', async () => {
+    let existingImage = container.querySelector('img')
+    existingImage.remove();
+
+    newDeck('new');
+})
+
+onStart();
